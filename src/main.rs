c251ad79b657f8634a032e1f5e0a5774271a93f4
Rafael Campos <racampos@gmail.com>
2024-05-15 20:08:57 -0600

    Progress

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use candle_core::scalar::TensorOrScalar;
 use candle_core::{DType, Device, IndexOp, Tensor};
+use candle_nn::encoding::one_hot;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::hash::Hash;
@@ -31,19 +33,78 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     chars.sort();
 
     let stoi =
-        HashMap::<char, i32>::from_iter(chars.iter().enumerate().map(|(i, &c)| (c, i as i32)));
+        HashMap::<char, i64>::from_iter(chars.iter().enumerate().map(|(i, &c)| (c, i as i64)));
+
+    let itos =
+        HashMap::<i64, char>::from_iter(chars.iter().enumerate().map(|(i, &c)| (i as i64, c)));
 
     let mut b: HashMap<(char, char), i32> = HashMap::new();
 
-    for word in words.iter() {
+    let mut xs: Vec<i64> = Vec::new();
+    let mut ys: Vec<i64> = Vec::new();
+
+    for word in words.iter().take(1) {
         for (ch1, ch2) in word.chars().zip(word.chars().skip(1)) {
             let ix1 = stoi.get(&ch1).unwrap();
             let ix2 = stoi.get(&ch2).unwrap();
-            N.slice_assign(ranges, src)
             let bigram = (ch1, ch2);
             b.insert(bigram, b.get(&bigram).unwrap_or(&0) + 1);
+            xs.push(*ix1);
+            ys.push(*ix2);
         }
     }
+
+    let W = Tensor::randn(0f32, 1., (27, 27), &Device::Cpu)?;
+
+    let xs = Tensor::from_vec(xs.clone(), xs.clone().len(), &Device::Cpu)?;
+    let ys = Tensor::from_vec(ys.clone(), ys.clone().len(), &Device::Cpu)?;
+    let xenc = one_hot(xs.clone(), 27, 1f32, 0f32);
+    let xenc = xenc.unwrap();
+    // println!("{:?}", xenc.to_vec2::<f32>());
+
+    let logits = xenc.matmul(&W)?;
+    let counts = logits.exp()?;
+    let counts_sum = counts.sum(1)?.unsqueeze(1)?.expand(&[5, 27])?;
+    let probs = counts.div(&counts_sum)?;
+
+    // println!("{:?}, shape: {:?}", probs.to_vec2::<f32>(), probs.shape());
+
+    let mut nlls: Vec<Tensor> = Vec::new();
+    for i in 0..5 {
+        let x = xs.get(i)?.to_vec0::<i64>()?;
+        let y = ys.get(i)?.to_vec0::<i64>()?;
+        println!("--------");
+        println!(
+            "bigram example {}: {}{}  (indexes {} {})",
+            i + 1,
+            itos.get(&x).unwrap(),
+            itos.get(&y).unwrap(),
+            x,
+            y
+        );
+        println!("input to the neural net: {:?}", x);
+        println!(
+            "output probabilities from the neural net: {:?}",
+            probs.get(i)?.to_vec1::<f32>()?
+        );
+        println!("label (actual next character): {:?}", y);
+        let p = probs.get(i)?.get(y.try_into().unwrap())?;
+        println!(
+            "probability assigned by the net to the correct character: {:?}",
+            p.to_vec0::<f32>()?
+        );
+        let logp = p.log()?;
+        println!("log likelihood: {:?}", logp.to_vec0::<f32>()?);
+        let nll = logp.neg()?;
+        println!("negative log likelihood: {:?}", nll.to_vec0::<f32>()?);
+        nlls.push(nll);
+    }
+    let nlls = Tensor::stack(&nlls, 0)?;
+    println!("==========");
+    println!(
+        "average negative log likelihood: {:?}",
+        nlls.mean(0)?.to_vec0::<f32>()?
+    );
 
     Ok(())
 }
