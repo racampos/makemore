11813e08b319698a515a5424e56c0ba3bbe0b913
Rafael Campos <racampos@gmail.com>
2024-05-29 12:00:18 -0600

    The model is now being trained using the SDG struct from the candle-nn crate.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use candle_core::scalar::TensorOrScalar;
-use candle_core::{DType, Device, IndexOp, Tensor};
+use candle_core::{DType, Device, Tensor, D};
 use candle_nn::encoding::one_hot;
+use candle_nn::{loss, ops, Module, Optimizer, VarBuilder, VarMap, SGD};
+use itertools::Itertools;
+use rand::distributions::WeightedIndex;
+use rand::prelude::*;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
-use std::hash::Hash;
-use std::io::{self, BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = "names.txt";
@@ -20,8 +22,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .iter()
         .map(|word| format!("{}{}{}", '.', word.to_string(), '.'))
         .collect::<Vec<String>>();
-
-    let N = Tensor::zeros((27, 27), DType::I64, &Device::Cpu)?;
 
     let mut unique_chars = HashSet::new();
     for word in words {
@@ -43,7 +43,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut xs: Vec<i64> = Vec::new();
     let mut ys: Vec<i64> = Vec::new();
 
-    for word in words.iter().take(1) {
+    for word in words.iter() {
         for (ch1, ch2) in word.chars().zip(word.chars().skip(1)) {
             let ix1 = stoi.get(&ch1).unwrap();
             let ix2 = stoi.get(&ch2).unwrap();
@@ -54,57 +54,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
 
-    let W = Tensor::randn(0f32, 1., (27, 27), &Device::Cpu)?;
+    let xs_len = xs.len();
+    let ys_len = ys.len();
+    let xs = Tensor::from_vec(xs.clone(), xs_len, &Device::Cpu)?;
+    let ys = Tensor::from_vec(ys.clone(), ys_len, &Device::Cpu)?;
 
-    let xs = Tensor::from_vec(xs.clone(), xs.clone().len(), &Device::Cpu)?;
-    let ys = Tensor::from_vec(ys.clone(), ys.clone().len(), &Device::Cpu)?;
     let xenc = one_hot(xs.clone(), 27, 1f32, 0f32);
     let xenc = xenc.unwrap();
-    // println!("{:?}", xenc.to_vec2::<f32>());
+    let varmap = VarMap::new();
+    let vs = VarBuilder::from_varmap(&varmap, DType::F32, &Device::Cpu);
+    let model = candle_nn::linear(27, 27, vs)?;
+    let mut sgd = SGD::new(varmap.all_vars(), 1.0)?;
+    const EPOCHS: i32 = 250;
+    let mut loss_scalar = 0.0;
 
-    let logits = xenc.matmul(&W)?;
-    let counts = logits.exp()?;
-    let counts_sum = counts.sum(1)?.unsqueeze(1)?.expand(&[5, 27])?;
-    let probs = counts.div(&counts_sum)?;
-
-    // println!("{:?}, shape: {:?}", probs.to_vec2::<f32>(), probs.shape());
-
-    let mut nlls: Vec<Tensor> = Vec::new();
-    for i in 0..5 {
-        let x = xs.get(i)?.to_vec0::<i64>()?;
-        let y = ys.get(i)?.to_vec0::<i64>()?;
-        println!("--------");
-        println!(
-            "bigram example {}: {}{}  (indexes {} {})",
-            i + 1,
-            itos.get(&x).unwrap(),
-            itos.get(&y).unwrap(),
-            x,
-            y
-        );
-        println!("input to the neural net: {:?}", x);
-        println!(
-            "output probabilities from the neural net: {:?}",
-            probs.get(i)?.to_vec1::<f32>()?
-        );
-        println!("label (actual next character): {:?}", y);
-        let p = probs.get(i)?.get(y.try_into().unwrap())?;
-        println!(
-            "probability assigned by the net to the correct character: {:?}",
-            p.to_vec0::<f32>()?
-        );
-        let logp = p.log()?;
-        println!("log likelihood: {:?}", logp.to_vec0::<f32>()?);
-        let nll = logp.neg()?;
-        println!("negative log likelihood: {:?}", nll.to_vec0::<f32>()?);
-        nlls.push(nll);
+    for epoch in 0..EPOCHS {
+        let logits = model.forward(&xenc)?;
+        let log_sm = ops::log_softmax(&logits, D::Minus1)?;
+        let loss = loss::nll(&log_sm, &ys)?;
+        sgd.backward_step(&loss)?;
+        loss_scalar = loss.to_scalar::<f32>()?;
+        if epoch % 10 == 0 {
+            println!("Epoch: {epoch:3} Train loss: {:8.5}", loss_scalar);
+        }
     }
-    let nlls = Tensor::stack(&nlls, 0)?;
-    println!("==========");
+
+    println!("\n==========\n");
     println!(
-        "average negative log likelihood: {:?}",
-        nlls.mean(0)?.to_vec0::<f32>()?
+        "Sample results after {} epochs of training with a training loss of {}:\n",
+        EPOCHS, loss_scalar
     );
+    let mut rng = thread_rng();
+
+    for i in 0..10 {
+        let mut out: Vec<&char> = Vec::new();
+        let mut ix: i64 = 0;
+        loop {
+            let ix_tensor = Tensor::from_vec(vec![ix], 1, &Device::Cpu)?;
+            let xenc = one_hot(ix_tensor, 27, 1f32, 0f32)?;
+            let logits = model.forward(&xenc)?;
+            let counts = logits.exp()?;
+            let counts_sum = counts.sum(1)?.unsqueeze(1)?.expand(&[1, 27])?;
+            let probs = counts.div(&counts_sum)?;
+            let probs_vec = probs.reshape(&[27])?.to_vec1::<f32>()?;
+            let dist = WeightedIndex::new(probs_vec).unwrap();
+            ix = dist.sample(&mut rng) as i64;
+            out.push(itos.get(&ix).unwrap());
+            if ix == 0 {
+                break;
+            }
+        }
+        println!("{:?}", out.iter().join(""));
+    }
 
     Ok(())
 }
